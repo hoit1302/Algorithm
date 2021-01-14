61e7c7034d8093899a7f6935944aa30c27ea7c92
hoit1302 <jueun000101@gmail.com>
2021-01-15 00:14:17 +0900

    N과 M (10)

---
+++ back_tracking/boj_15664.java
@@ -0,0 +1,50 @@
+package back_tracking;
+
+import java.io.*;
+import java.util.*;
+
+public class boj_15664 {
+
+    static StringBuilder sb;
+    static Stack<Integer> stack;
+    static int[] nums;
+    static int N, M;
+    static boolean[] visited;
+
+    private static void func(int order, int startIndex) {
+        if (order == M + 1) {
+            stack.forEach((num) -> sb.append(num).append(" "));
+            sb.append("\n");
+            return;
+        }
+        int lastCall = 0;
+        for (int index = startIndex; index < N; index++) {
+            if (!visited[index]) {
+                if (lastCall == nums[index]){
+                    continue;
+                } lastCall = nums[index];
+                stack.push(nums[index]);
+                visited[index] = true;
+                func(order + 1, index+1);
+                stack.pop();
+                visited[index] = false;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        sb = new StringBuilder();
+        stack = new Stack<>();
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        visited = new boolean[N]; // 모두 false
+        nums = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        Arrays.sort(nums);
+        func(1, 0);
+        bw.write(sb.toString());
+        bw.flush();
+    }
+}
