097a5ff57c7734e4d1f572f257c79e591d942f1a
hoit1302 <jueun000101@gmail.com>
2021-01-25 19:24:08 +0900

    1로 만들기 - 시간 줄임

--- dp/boj_1463.java
+++ dp/boj_1463.java
@@ -14,7 +14,7 @@ public class boj_1463 {
 		for (int i = 4; i <= N; i++) {
 			dp[i] = dp[i - 1] + 1;
 			if (i % 3 == 0 && dp[i / 3]+1 < dp[i]) dp[i] = dp[i / 3] + 1;
-			if (i % 2 == 0 && dp[i / 2] +1< dp[i]) dp[i] = dp[i / 2] + 1;
+			else if (i % 2 == 0 && dp[i / 2] +1< dp[i]) dp[i] = dp[i / 2] + 1;
 		}
 		bw.write(String.valueOf(dp[N]));
 		bw.flush();
